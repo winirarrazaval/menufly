05ee6f5d1634923863113fe09c66b3de50b29e44
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-07-03 21:43:21 -0700

    Initial Commit

---
+++ menufly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  menufly
+//
+//  Created by winifred irarrazaval Oehninger on 7/3/18.
+//  Copyright © 2018 winifred irarrazaval Oehninger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
